a3e31836289ac4db42f09a65b344abd3a29ddab8
Administrator <Administrator@115-68-21-132>
2015-12-23 23:16:00 +0900

    쿠폰 발송 테스트 작업 중

---
+++ hli/src/main/java/com/hli/controller/CouponController.java
@@ -0,0 +1,48 @@
+package com.hli.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.ibatis.exceptions.PersistenceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.hli.domain.CouponReqVO;
+
+@RestController
+public class CouponController {
+	private static Logger logger = LoggerFactory.getLogger(AdminController.class);
+	private RestTemplate restTemplate;
+	private Map<String, Object> params;
+	
+	//쿠폰 발송 OPEN API=====================================================================
+	@RequestMapping("/coupon/api/v1/sendCoupons")
+	public String sendCoupons(@RequestBody CouponReqVO couponReq) {
+		logger.debug("/api/v1/sendCoupons------------------------------------------------------------");
+		
+		restTemplate = new RestTemplate();
+		//coup handling
+		if (couponReq.getProvider() == 2) {
+			params = new HashMap<>();
+			params.put("CODE", "0424");
+			params.put("PASS", "hlint123");
+			params.put("COUPONCODE", couponReq.getGoods_code());
+			params.put("SEQNUMBER", couponReq.getTr_id());
+			params.put("QTY", couponReq.getGoods_count());
+			params.put("HP", couponReq.getRecv_phone());
+			params.put("CALLBACK", couponReq.getSend_phone());
+			params.put("ADDMSG", couponReq.getMessage());
+			params.put("SELPRICE", couponReq.getSell_price());
+		}
+		
+		Object result = restTemplate.getForEntity(
+				"http://issuev3apitest.m2i.kr:9999/serviceapi_02.asmx/ServiceCreateSendMuch",
+				Object.class,
+				params);
+		return result.toString();
+	}
+}
---
+++ hli/src/main/java/com/hli/domain/CouponReqVO.java
@@ -0,0 +1,15 @@
+package com.hli.domain;
+
+import lombok.Data;
+
+@Data
+public class CouponReqVO {
+	private int provider; //공급업체 1:M12, 2:coup
+	private String goods_count;  //M12(max:5) coup(max:10)
+	private String sell_price;   //판매가격
+	private String goods_code;
+	private String send_phone; //발신자
+	private String recv_phone; //수신자
+	private String message;
+	private String tr_id; //거래코드
+}
