d14c0ce8fc5eb91e7154a99ef85cbfec39e17613
Administrator <Administrator@115-68-21-132>
2015-12-27 23:29:39 +0900

    coup 쿠폰 발송 API 연동

--- hli/src/main/java/com/hli/controller/CouponController.java
+++ hli/src/main/java/com/hli/controller/CouponController.java
@@ -1,19 +1,29 @@
 package com.hli.controller;
 
+import java.io.StringReader;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.naming.StringRefAddr;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.ibatis.exceptions.PersistenceException;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.filter.ElementFilter;
+import org.jdom.input.SAXBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.hli.domain.CouponReqVO;
 import com.hli.domain.GoodsVO;
@@ -27,13 +37,13 @@ import com.hli.service.AdminService;
 
 @RestController
 public class CouponController {
-	private static Logger logger = LoggerFactory.getLogger(AdminController.class);
+	private static Logger logger = LoggerFactory.getLogger(CouponController.class);
 	
 	@Autowired
 	private AdminService adminService;
 	
 	private RestTemplate restTemplate;
-	private Map<String, Object> params;
+	private MultiValueMap<String, String> params;
 	
 	//판매업체에게 상품을 공급해주는 REST API
 	@RequestMapping("/api/getAllGoodsOfSeller")
@@ -63,28 +73,50 @@ public class CouponController {
 	
 	//쿠폰 발송 OPEN API=====================================================================
 	@RequestMapping("/coupon/api/v1/sendCoupons")
-	public String sendCoupons(@RequestBody CouponReqVO couponReq) {
+	public Result sendCoupons(@RequestBody CouponReqVO couponReq) {
 		logger.debug("/api/v1/sendCoupons------------------------------------------------------------");
 		
 		restTemplate = new RestTemplate();
+		Result result = new Result();
 		//coup handling
 		if (couponReq.getProvider() == 2) {
-			params = new HashMap<>();
-			params.put("CODE", "0424");
-			params.put("PASS", "hlint123");
-			params.put("COUPONCODE", couponReq.getGoods_code());
-			params.put("SEQNUMBER", couponReq.getTr_id());
-			params.put("QTY", couponReq.getGoods_count());
-			params.put("HP", couponReq.getRecv_phone());
-			params.put("CALLBACK", couponReq.getSend_phone());
-			params.put("ADDMSG", couponReq.getMessage());
-			params.put("SELPRICE", couponReq.getSell_price());
+			params = new LinkedMultiValueMap<String, String>();
+			params.add("CODE", "0424");
+			params.add("PASS", "hlint123");
+			params.add("COUPONCODE", couponReq.getGoods_code());
+			params.add("SEQNUMBER", couponReq.getTr_id());
+			params.add("QTY", couponReq.getGoods_count());
+			params.add("HP", couponReq.getRecv_phone());
+			params.add("CALLBACK", couponReq.getSend_phone());
+			params.add("TITLE", "");
+			params.add("ADDMSG", couponReq.getMessage());
+			params.add("SELPRICE", couponReq.getSell_price());
+			
+		}
+		//logger.debug(params.toString());
+		String baseUrl = "http://issuev3apitest.m2i.kr:9999/serviceapi_02.asmx/ServiceCreateSendMuch";
+		try {
+			UriComponents uriComponents = UriComponentsBuilder.fromHttpUrl(baseUrl).queryParams(params).build();
+			logger.debug(uriComponents.toUriString());
+			String strResult = restTemplate.getForObject(
+					uriComponents.toUriString(),
+					String.class);
+			SAXBuilder builder = new SAXBuilder();
+			Document document = (Document) builder.build(new StringReader(strResult));
+			Element rootNode = document.getRootElement();
+			logger.debug(rootNode.getName());
+			String resultCode = rootNode.getChild("RESULTCODE", rootNode.getNamespace()).getText();
+			String resultMsg = rootNode.getChild("RESULTMSG", rootNode.getNamespace()).getText();
+			logger.debug(resultCode);
+			logger.debug(resultMsg);
+			result.setResult(Integer.parseInt(resultCode));
+			result.setMsg(resultMsg);
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+			return new Result(100, "Coupon failed");
 		}
 		
-		Object result = restTemplate.getForEntity(
-				"http://issuev3apitest.m2i.kr:9999/serviceapi_02.asmx/ServiceCreateSendMuch",
-				Object.class,
-				params);
-		return result.toString();
+		return result;
 	}
 }
