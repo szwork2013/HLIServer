1006071380c91e4c538a6c2352afe4e1a2731d37
Administrator <Administrator@115-68-21-132>
2015-12-28 23:23:50 +0900

    M12 쿠폰 발송 API 연동

--- hli/src/main/java/com/hli/controller/CouponController.java
+++ hli/src/main/java/com/hli/controller/CouponController.java
@@ -5,13 +5,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.naming.StringRefAddr;
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.ibatis.exceptions.PersistenceException;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.jdom.Document;
 import org.jdom.Element;
-import org.jdom.filter.ElementFilter;
 import org.jdom.input.SAXBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,13 +27,11 @@ import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.hli.domain.CouponReqVO;
-import com.hli.domain.GoodsVO;
 import com.hli.domain.SearchVO;
 import com.hli.domain.SellerGoodsVO;
 import com.hli.domain.SellerVO;
 import com.hli.result.Result;
 import com.hli.result.ResultData;
-import com.hli.result.ResultDataTotal;
 import com.hli.service.AdminService;
 
 @RestController
@@ -75,12 +74,68 @@ public class CouponController {
 	@RequestMapping("/coupon/api/v1/sendCoupons")
 	public Result sendCoupons(@RequestBody CouponReqVO couponReq) {
 		logger.debug("/api/v1/sendCoupons------------------------------------------------------------");
-		
+
 		restTemplate = new RestTemplate();
 		Result result = new Result();
-		//coup handling
-		if (couponReq.getProvider() == 2) {
-			params = new LinkedMultiValueMap<String, String>();
+		params = new LinkedMultiValueMap<String, String>();
+		UriComponents uriComponents;
+		String baseUrl = "";
+		//M12 handling
+		if (couponReq.getProvider() == 1) {
+			params.add("goods_code", couponReq.getGoods_code());
+			params.add("goods_count", couponReq.getGoods_count());
+			params.add("send_phone", couponReq.getSend_phone());
+			params.add("recv_phone", couponReq.getRecv_phone());
+			params.add("tr_id", couponReq.getTr_id());
+			params.add("userid", "banyoungmo");
+			params.add("sell_price", couponReq.getSell_price());
+			params.add("msg", couponReq.getMessage());
+
+			baseUrl = "http://web6.m12.co.kr:12101/app/dev/order_send.php";
+			uriComponents = UriComponentsBuilder.fromHttpUrl(baseUrl).queryParams(params).build();
+			String value = uriComponents.getQuery();
+			String key = "HLINTNLE54A3I2O1";
+			String initVector = "J0S9O8T7USJFDLSX";
+			logger.debug(value);
+
+			try {
+
+				IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+				SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+				Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+				cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+
+				byte[] encrypted = cipher.doFinal(value.getBytes());
+				String strpara = Base64.encodeBase64String(encrypted);
+				params.clear();
+				params.add("marketcode", "HLINTNL01");
+				params.add("strpara", strpara);
+				uriComponents = UriComponentsBuilder.fromHttpUrl(baseUrl).queryParams(params).build();
+				logger.debug(uriComponents.toUriString());
+				String strResult = restTemplate.getForObject(
+						uriComponents.toUriString(),
+						String.class);
+				SAXBuilder builder = new SAXBuilder();
+				Document document = (Document) builder.build(new StringReader(strResult));
+				Element rootNode = document.getRootElement();
+				logger.debug(rootNode.getName());
+				String resultCode = rootNode.getChild("RESULT_CODE", rootNode.getNamespace()).getText();
+				String resultMsg = rootNode.getChild("STATUS_CODE", rootNode.getNamespace()).getText();
+				logger.debug(resultCode);
+				logger.debug(resultMsg);
+				result.setResult(Integer.parseInt(resultCode));
+				result.setMsg(resultMsg);
+			}
+
+			catch(Exception e) {
+				e.printStackTrace();
+				return new Result(100, "Coupon failed");
+			}
+			return result;
+		}
+		//Coup handling
+		else if (couponReq.getProvider() == 2) {
 			params.add("CODE", "0424");
 			params.add("PASS", "hlint123");
 			params.add("COUPONCODE", couponReq.getGoods_code());
@@ -91,32 +146,37 @@ public class CouponController {
 			params.add("TITLE", "");
 			params.add("ADDMSG", couponReq.getMessage());
 			params.add("SELPRICE", couponReq.getSell_price());
-			
+
+
+			//logger.debug(params.toString());
+			baseUrl = "http://issuev3apitest.m2i.kr:9999/serviceapi_02.asmx/ServiceCreateSendMuch";
+			try {
+				uriComponents = UriComponentsBuilder.fromHttpUrl(baseUrl).queryParams(params).build();
+				logger.debug(uriComponents.toUriString());
+				String strResult = restTemplate.getForObject(
+						uriComponents.toUriString(),
+						String.class);
+				SAXBuilder builder = new SAXBuilder();
+				Document document = (Document) builder.build(new StringReader(strResult));
+				Element rootNode = document.getRootElement();
+				logger.debug(rootNode.getName());
+				String resultCode = rootNode.getChild("RESULTCODE", rootNode.getNamespace()).getText();
+				String resultMsg = rootNode.getChild("RESULTMSG", rootNode.getNamespace()).getText();
+				logger.debug(resultCode);
+				logger.debug(resultMsg);
+				result.setResult(Integer.parseInt(resultCode));
+				result.setMsg(resultMsg);
+			} 
+			catch (Exception e) {
+				e.printStackTrace();
+				return new Result(100, "Coupon failed");
+			}
+
 		}
-		//logger.debug(params.toString());
-		String baseUrl = "http://issuev3apitest.m2i.kr:9999/serviceapi_02.asmx/ServiceCreateSendMuch";
-		try {
-			UriComponents uriComponents = UriComponentsBuilder.fromHttpUrl(baseUrl).queryParams(params).build();
-			logger.debug(uriComponents.toUriString());
-			String strResult = restTemplate.getForObject(
-					uriComponents.toUriString(),
-					String.class);
-			SAXBuilder builder = new SAXBuilder();
-			Document document = (Document) builder.build(new StringReader(strResult));
-			Element rootNode = document.getRootElement();
-			logger.debug(rootNode.getName());
-			String resultCode = rootNode.getChild("RESULTCODE", rootNode.getNamespace()).getText();
-			String resultMsg = rootNode.getChild("RESULTMSG", rootNode.getNamespace()).getText();
-			logger.debug(resultCode);
-			logger.debug(resultMsg);
-			result.setResult(Integer.parseInt(resultCode));
-			result.setMsg(resultMsg);
-		} 
-		catch (Exception e) {
-			e.printStackTrace();
-			return new Result(100, "Coupon failed");
+		else {
+			result.setResult(100);
+			result.setMsg("Unknown provider");
 		}
-		
 		return result;
 	}
 }
